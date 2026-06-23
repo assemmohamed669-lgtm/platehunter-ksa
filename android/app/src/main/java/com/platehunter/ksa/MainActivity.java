96f118d181e28cdbc78955319f6996d583756ced
assemmohamed669-lgtm <assemmohamed669-lgtm@users.noreply.github.com>
2026-06-24 00:47:08 +0400

    fix: enable geolocation in WebView settings

--- android/app/src/main/java/com/platehunter/ksa/MainActivity.java
+++ android/app/src/main/java/com/platehunter/ksa/MainActivity.java
@@ -3,6 +3,7 @@ package com.platehunter.ksa;
 import android.webkit.GeolocationPermissions;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
@@ -11,17 +12,20 @@ public class MainActivity extends BridgeActivity {
     public void onStart() {
         super.onStart();
         WebView webView = getBridge().getWebView();
+
+        // Enable geolocation in the WebView
+        WebSettings settings = webView.getSettings();
+        settings.setGeolocationEnabled(true);
+
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onPermissionRequest(PermissionRequest request) {
-                // Grant microphone, camera, etc.
                 request.grant(request.getResources());
             }
 
             @Override
             public void onGeolocationPermissionsShowPrompt(
                     String origin, GeolocationPermissions.Callback callback) {
-                // Grant location access to the web page
                 callback.invoke(origin, true, false);
             }
         });
