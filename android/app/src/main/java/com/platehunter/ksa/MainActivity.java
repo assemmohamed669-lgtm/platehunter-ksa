a02a03cad6b4ac488f228dd90622fddb908537a0
assemmohamed669-lgtm <assemmohamed669-lgtm@users.noreply.github.com>
2026-06-24 00:44:41 +0400

    fix: grant microphone and geolocation permissions in WebView

--- android/app/src/main/java/com/platehunter/ksa/MainActivity.java
+++ android/app/src/main/java/com/platehunter/ksa/MainActivity.java
@@ -1,5 +1,29 @@
 package com.platehunter.ksa;
 
+import android.webkit.GeolocationPermissions;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        WebView webView = getBridge().getWebView();
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                // Grant microphone, camera, etc.
+                request.grant(request.getResources());
+            }
+
+            @Override
+            public void onGeolocationPermissionsShowPrompt(
+                    String origin, GeolocationPermissions.Callback callback) {
+                // Grant location access to the web page
+                callback.invoke(origin, true, false);
+            }
+        });
+    }
+}
